5169046f557644c7097fb88b42eaae49572ab889
Roy Andres <rhavila789@gmail.com>
2024-09-16 13:15:33 -0500

    Add .env and data directories to .gitignore

--- src/preload.js
+++ src/preload.js
@@ -2,4 +2,5 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("files", {
   readFiles: () => ipcRenderer.invoke("readFiles"),
+  extractTextFromImage: () => ipcRenderer.invoke("extractTextFromImage"),
 });
